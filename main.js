26ebc0f3aa0412a05946be7421791b3b815656eb
ivantarabanovych <vanjokcitet334@gmail.com>
2024-08-26 21:06:02 +0300

    q3

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-//Напишіть функцію, яка приймає масив чисел і використовує функцію вищого порядку,
-//наприклад reduce, для обчислення суми всіх чисел у масиві.
-
-const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8 , 9];
-
-function sumArray(numbers){
-    return numbers.reduce((sum, numbers) => sum + numbers, 0)
+function replacePlace(text, oldPhrase, newPhrase) {
+    const phrase = new RegExp(oldPhrase, 'g'); 
+    return text.replace(phrase, newPhrase);    
 }
 
-const sum = sumArray(numbersArray);
-console.log(sum); // 4️⃣5️⃣
+const originalText = "Це тестовий рядок. Тестовий рядок повинен бути змінений.";
+const modifiedText = replacePlace(originalText, "тестовий", "змінений");
+
+console.log(modifiedText);
